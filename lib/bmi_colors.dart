f32c9e18dd483fa792b5e43d24790c6f317bf7e1
AFSNAMVA <efsanememmedova073@gmail.com>
2024-04-08 00:16:56 +0300

    first commit

--- lib/bmi_colors.dart
+++ lib/bmi_colors.dart
@@ -12,4 +12,5 @@ class AppColors {
   static const Color yellow= Colors.yellow;
   static const Color red= Colors.red;
   static const Color grey= Colors.grey;
+  static const Color black= Colors.black;
 }
